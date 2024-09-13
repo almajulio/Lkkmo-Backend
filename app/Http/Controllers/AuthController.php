623a56e6df088d72dc8ef4d56099e0440529e8bb
almajulioo <alma.122140057@student.itera.ac.id>
2024-09-13 13:18:11 +0700

    feat: add login and register in authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+
+        $data = [
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => $request->password,
+        ];
+
+        if(Auth::attempt($data)){
+            return response()->json([
+                'status' => 200,
+                'message' => 'Login Successfully',
+            ]);
+        }else{
+            return response()->json([
+                'status' => 401,
+                'message' => 'Login Failed',
+            ]);
+        }
+       
+    }
+
+    public function register(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+        
+        User::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+            'role_id' => 1
+        ]);
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Success',
+            'data' => $products
+        ]);
+    }
+
+}
