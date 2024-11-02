f36f62f3ec714fa2eb3b011417162dec27d7b0b5
almajulioo <alma.122140057@student.itera.ac.id>
2024-11-02 23:44:39 +0700

    update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -33,6 +33,6 @@ class Product extends Model
     }
     public function reviews()
     {
-        return $this->hasMany(Review::class);
+        return $this->hasMany(Review::class)->with('user');
     }
 }
